e04acf9688a6e4c072060c7f7c28ed683a0a6a5b
Ana <anacaroline@teste.com>
2024-10-10 20:56:05 -0300

    remocao console.log

--- main.js
+++ main.js
@@ -5,8 +5,6 @@ form.addEventListener('submit', function(e) {
     e.preventDefault();
     const campoB = parseFloat(document.getElementById('campo-b').value);
     const campoA = parseFloat(document.getElementById('campo-a').value);
-    console.log (campoA , campoB)
-    console.log (campoB > campoA)
     const mensagem = campoB > campoA ? 'O campo B é maior que o campo A' : 'O campo B não é maior que o campo A'
     return alert (mensagem)
 
