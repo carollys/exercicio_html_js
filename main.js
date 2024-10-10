c18b38af9f8d79b047d9e1bb22697019a1fddb6b
Ana <anacaroline@teste.com>
2024-10-10 20:48:22 -0300

    correção do código

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
 const form = document.getElementById('form-validacao');
-let formEValido = false;
 
 
 form.addEventListener('submit', function(e) {
     e.preventDefault();
-    const campoB = document.getElementById ('campo-b').value;
-    const campoA = document.getElementById ('campo-a').value;
+    const campoB = parseFloat(document.getElementById('campo-b').value);
+    const campoA = parseFloat(document.getElementById('campo-a').value);
+    console.log (campoA , campoB)
+    console.log (campoB > campoA)
     const mensagem = campoB > campoA ? 'O campo B é maior que o campo A' : 'O campo B não é maior que o campo A'
     return alert (mensagem)
 
